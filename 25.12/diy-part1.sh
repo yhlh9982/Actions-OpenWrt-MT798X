d788d185e6f2ffc07179562c5152a62deeb22295
Otto kai <121492489+yhlh9982@users.noreply.github.com>
2026-04-16 16:58:30 +0800

    Update diy-part1.sh

--- 25.12/diy-part1.sh
+++ 25.12/diy-part1.sh
@@ -10,54 +10,44 @@
 # See /LICENSE for more information.
 #
 
-# 备份原始 feeds
-cp feeds.conf.default feeds.conf.default.bak
-
 # 科学插件
-# Passwall 
-# echo 'src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git' >> feeds.conf.default
-echo 'src-git passwall https://github.com/Openwrt-Passwall/openwrt-passwall.git' >> feeds.conf.default
-echo 'src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git' >> feeds.conf.default
-# OpenClash 
-echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >> feeds.conf.default
+# Passwall
+# git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/custom/passwall-packages
+git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall package/custom/passwall
+git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall2 package/custom/passwall2
+
+# OpenClash
+git clone --depth=1 -b dev https://github.com/vernesong/OpenClash.git package/custom/openclash
+
 # Nikki / Momo
-echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git' >> feeds.conf.default
-echo 'src-git momo https://github.com/nikkinikki-org/OpenWrt-momo.git' >> feeds.conf.default
-# Daed (指定 kix 分支)
-echo 'src-git daed https://github.com/QiuSimons/luci-app-daed.git;kix' >> feeds.conf.default
-# Helloworld (SSR+)
-echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
+git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki.git package/custom/nikki
+git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-momo.git package/custom/momo
 
-# === 功能插件 Feeds ===
-# Lucky (大吉大利，端口转发/反向代理神器)
-echo 'src-git lucky https://github.com/gdy666/luci-app-lucky.git' >> feeds.conf.default
-# Openlist2 (可能是指 OpenList 或相关列表工具)
-echo 'src-git openlist2 https://github.com/sbwml/luci-app-openlist2.git' >> feeds.conf.default
+# Daed
+git clone --depth=1 -b kix https://github.com/QiuSimons/luci-app-daed.git package/custom/daed
 
-# Sirpdboy 的插件 (看门狗 & 计划任务)
-echo 'src-git watchdog https://github.com/sirpdboy/luci-app-watchdog.git' >> feeds.conf.default
-echo 'src-git taskplan https://github.com/sirpdboy/luci-app-taskplan.git' >> feeds.conf.default
-# Authshield (认证屏蔽/管理)
-echo 'src-git authshield https://github.com/iv7777/luci-app-authshield.git' >> feeds.conf.default
+# SSR+
+git clone --depth=1 https://github.com/fw876/helloworld.git package/custom/ssrp
+
+# 功能插件
+git clone --depth=1 https://github.com/gdy666/luci-app-lucky.git package/custom/lucky
+git clone --depth=1 https://github.com/sbwml/luci-app-openlist2.git package/custom/openlist2
+
+git clone --depth=1 https://github.com/sirpdboy/luci-app-watchdog.git package/custom/watchdog
+git clone --depth=1 https://github.com/sirpdboy/luci-app-taskplan.git package/custom/taskplan
+
+git clone --depth=1 https://github.com/iv7777/luci-app-authshield.git package/custom/authshield
 
 # VPN
-# EasyTier (内网穿透组网)
-echo 'src-git easytier https://github.com/EasyTier/luci-app-easytier.git' >> feeds.conf.default
-# Tailscale Community (注意：改名为 tailscale_community 以防与官方 packages 里的 tailscale 混淆)
-echo 'src-git tailscale_community https://github.com/Tokisaki-Galaxy/luci-app-tailscale-community.git' >> feeds.conf.default
-# OWQ WOL (网络唤醒)
-echo 'src-git owq_wol https://github.com/isalikai/luci-app-owq-wol.git' >> feeds.conf.default
-
-mkdir -p package/custom
+git clone --depth=1 https://github.com/EasyTier/luci-app-easytier.git package/custom/easytier
+git clone --depth=1 https://github.com/Tokisaki-Galaxy/luci-app-tailscale-community.git package/custom/tailscale-community
+git clone --depth=1 https://github.com/isalikai/luci-app-owq-wol.git package/custom/owq-wol
 
 # 主题
-# Argon
 git clone --depth=1 -b openwrt-25.12 https://github.com/sbwml/luci-theme-argon.git package/custom/luci-theme-argon
 
-# Aurora
 git clone --depth=1 https://github.com/eamonxg/luci-theme-aurora.git package/custom/luci-theme-aurora
 git clone --depth=1 https://github.com/eamonxg/luci-app-aurora-config.git package/custom/luci-app-aurora-config
 
-# Kucat
 git clone --depth=1 https://github.com/sirpdboy/luci-theme-kucat.git package/custom/luci-theme-kucat
 git clone --depth=1 https://github.com/sirpdboy/luci-app-kucat-config.git package/custom/luci-app-kucat-config
