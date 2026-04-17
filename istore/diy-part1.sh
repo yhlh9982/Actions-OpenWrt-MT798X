076e3323c1d7b96452e368e6d45af96a2c8d560e
Otto kai <121492489+yhlh9982@users.noreply.github.com>
2026-04-17 22:23:30 +0800

    Update diy-part1.sh

--- istore/diy-part1.sh
+++ istore/diy-part1.sh
@@ -14,20 +14,11 @@
 cp feeds.conf.default feeds.conf.default.bak
 
 # 2. NAS feeds
-echo >> feeds.conf.default
 echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
 echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
 
-./scripts/feeds update nas nas_luci
-./scripts/feeds install -a -p nas
-./scripts/feeds install -a -p nas_luci
-
 # 3. iStore feeds
-echo >> feeds.conf.default
 echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
-
-./scripts/feeds update istore
-./scripts/feeds install -d y -p istore luci-app-store
 
 mkdir -p package/custom
 
